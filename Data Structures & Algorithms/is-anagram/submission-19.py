3b01b057e7d0f384912bcfca4c9ba2de03d186e3
Syed Ahamed jaafri <jaafri474@gmail.com>
2026-07-04 13:26:02 +0530

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        #   BRUTE FORCE METHOD
+
+        # checking length 
+        if len(s) != len(t):
+            return False
+        
+        # for checking all the characters in both strings are same 
+
+        # counting each character in both the strings and checking if they are same
+
+        # creating dictionaries/ hashmaps
+        counts , countt  = {} , {}
+        for i in range(len(s)):
+            counts[s[i]] = 1 + counts.get(s[i] , 0) # counts the characters in s
+            countt[t[i]] = 1 + countt.get(t[i] , 0)  # counts characters in t
+            
+        for j in counts:
+            if counts[j] != countt.get(j , 0):
+                return False
+        return True
+    
+
