ef8b31affe629ae35bd0d06ae35470e623b69aa1
Syed Ahamed jaafri <jaafri474@gmail.com>
2026-08-22 23:51:41 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # the question is if we a change one variable to most repeating duplicate value , what's the  final long repeating length , but we can change based on what k is ?
+
+    # brute force using hash map 
+        count = {}
+        res = 0
+        l = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r] , 0)
+
+            while(r - l+1)  - max(count.values()) > k: # ws - max count
+                count[s[l]] -= 1   #when the ws - max count exceed k condition , remove the l pointer and increment it to 1
+                l = l+1
+            res = max(res , r- l+1)
+        return res
